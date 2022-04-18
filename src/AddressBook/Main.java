58b0ddaedb73b87669dada31665646940c815f95
Artur Wierzba <artur.w.wierzba@gmail.com>
2022-04-18 20:50:34 +0200

    upgrade

--- src/AddressBook/Main.java
+++ src/AddressBook/Main.java
@@ -1,17 +1,5 @@
 package AddressBook;
 
-// TODO Bilbo challange: Napisanie aplikacji, która będzie robiła książkę adresową, żebyś mógł dodawać nowe kontakty, wyświetlać listę kontaktów, wyświetlić pojedynczy kontakt, usunąć kontakt (klasa ArrayList)
-
-/* Plan aplikacji:
-
-Funkcjonalności:
-- Wyświetlanie wszystkich kontaktów
-- Wyświetlanie pojedynczego kontaktu
-- Dodawania nowego kontaktu
-- Usuwanie kontaktu
-
- */
-
 // TODO format code for refactoring - swap number for variables declared at the very beginning of the code
 
 import java.util.ArrayList;
@@ -20,18 +8,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        int contactId = 1, button1 = 1, button2 = 2, button3 = 3, button4 = 4;
+
         ArrayList<String> contacts = new ArrayList<>();
-        contacts.add("Jerry Springer");
-        contacts.add("Jeff Bezos");
+        contacts.add(contactId++ + ". " + "Jerry Springer ");
+        contacts.add(contactId++ + ". " + "Jeff Bezos ");
 
         Scanner scanner = new Scanner(System.in);
 
         boolean cont = true;
-
-        int button1 = 1;
-        int button2 = 2;
-        int button3 = 3;
-        int button4 = 4;
 
         while (cont) {
             System.out.println("Hello!");
@@ -44,30 +29,33 @@ public class Main {
 
             if (number == button1) {
                 System.out.println(contacts);
+                scanner.nextLine();
+
             } else if (number == button2) {
                 System.out.println("Please provide an ordinal number of a contact to be shown");
+                int contactID = scanner.nextInt();
+                System.out.println(contacts.get(--contactID));
+                scanner.nextLine();
 
             } else if (number == button3) {
                 System.out.println("Please provide first name and last name of new contact");
                 scanner.nextLine();
                 String contactName = scanner.nextLine();
-                contacts.add(contactName);
+                contacts.add(contactId++ + ". " + contactName);
                 System.out.println("Contact has been added successfully!");
 
             } else if (number == button4) {
-                // TODO I should add an ordinal number to make the deletion and showing one contact easier
-                System.out.println("Please provide first name and last name of contact to be deleted");
-                scanner.nextLine();
-                String contactName = scanner.nextLine();
-                contacts.add(contactName);
+                System.out.println("Please provide an ordinal number of a contact to be deleted");
+                int contactID = scanner.nextInt();
+                contacts.remove(--contactID);
                 System.out.println("Contact has been successfully deleted!");
+                scanner.nextLine();
+
+                //TODO test operations out of scope of regular use
             } else {
-                // TODO after operation finished - "Do you wish to go back to the main menu?" Y/N
                 System.out.println("Operation impossible to perform");
-                cont = false;
             }
             System.out.println("Do you wish to go back to the main menu? Press Y/N");
-            scanner.nextLine();
             String backToMenu = scanner.nextLine();
             switch (backToMenu) {
                 case "Y":
@@ -75,17 +63,6 @@ public class Main {
                 case "N":
                     cont = false;
             }
-
-
-//        if (number == 1) {
-//            System.out.println(contacts);
-//        } else if (number == 2) {
-//            System.out.println("Please provide first name and last name of new contact");
-//            String contactName = scanner.nextLine();
-//            contacts.add(contactName);
-//        } else {
-//            System.out.println("Operation impossible to perform");
-
         }
     }
 }
